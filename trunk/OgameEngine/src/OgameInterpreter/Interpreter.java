5f3c7200716d4b8e4d1b8aca250fcdaf21bdf257
dyschemist <dyschemist@ac2fc3db-d806-6336-2a2a-3cf1882426c2>
2012-07-21 20:12:40 +0000

    Pierwsze próby interpretera

--- trunk/OgameEngine/src/OgameInterpreter/Interpreter.java
+++ trunk/OgameEngine/src/OgameInterpreter/Interpreter.java
@@ -4,7 +4,10 @@
  */
 package OgameInterpreter;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
@@ -24,11 +27,25 @@ public class Interpreter {
         String[] temp;
         for (int i=0; i< str.length; i++){
             temp = str[i].split(";");
+            if (0==TimePrefixes.compareStringAndPrefix(temp[0].substring(0, 2), TimePrefixes.AT) ){
+                System.out.println("AT");
+            } else if (0==TimePrefixes.compareStringAndPrefix(temp[0].substring(0, 2), TimePrefixes.ON) ){
+                System.out.println("ON"); 
+            } else if (0==TimePrefixes.compareStringAndPrefix(temp[0].substring(0, 2), TimePrefixes.NW) ){
+                System.out.println("NW");
+            } else if (0==TimePrefixes.compareStringAndPrefix(temp[0].substring(0, 2), TimePrefixes.IN) ){
+                System.out.println("IN");
+            } else {
+                System.out.println("NONE");
+            }
             map.put(string2Calendar(temp[0]), temp[1]);
         }
     }
+    static DateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+    static DateFormat fulldf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    
     private Calendar string2Calendar(String s){
-    return null;
+    return GregorianCalendar.getInstance();
     }
     private Calendar interpretTime(String str){
         
