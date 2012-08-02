1600f6f7edbd6f925b940a2be1b28e10b782c0b9
dyschemist <dyschemist@ac2fc3db-d806-6336-2a2a-3cf1882426c2>
2012-08-02 17:50:33 +0000

    poprawione properties

--- OgameGuard/src/ogameguard/OgameGuard.java
+++ OgameGuard/src/ogameguard/OgameGuard.java
@@ -7,11 +7,14 @@ package ogameguard;
 import OgameEngine.Flights;
 import OgameEngine.Ogame;
 import OgameEngine.OgameException;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Iterator;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -20,9 +23,11 @@ import java.util.List;
 public class OgameGuard {
 
     Ogame o;
+    static GuardProperties properties;
 
     public OgameGuard(Ogame o) {
         this.o = o;
+        
     }
     
     
@@ -31,17 +36,11 @@ public class OgameGuard {
     }
     
     
-    List<Flights> getEventList() throws OgameException{
-        return o.getEventList(); 
-    }
-    
     static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd  HH:mm:ss");
     
     void check() throws OgameException {
-            String uni = "Pegasus", nick ="Mystic", pass="SuperUltraSsjGoku2";
             Calendar now = GregorianCalendar.getInstance();
-            this.login(uni, nick, pass);
-            List<Flights> list = this.getEventList();
+            List<Flights> list = this.o.getEventList(); 
             Iterator<Flights> it = list.iterator();
             Flights temp;
             boolean isOkey = true;
@@ -60,7 +59,42 @@ public class OgameGuard {
             if (isOkey){
                 System.out.println(sdf.format(now.getTime())+"->Wszystko w porządku");
             }
-            this.o.logout();
+            
+    }
+    
+    public static void main1(){
+        try {
+                properties = new GuardProperties();
+            } catch (IOException ex) {
+                Logger.getLogger(OgameGuard.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            String uni_number = properties.getUni_number();
+            String lang= properties.getUni_lang();
+            int hour=0,minute=2,seconds=0;
+            OgameGuard guard = new OgameGuard(Ogame.ogameFabric(uni_number, lang));
+            String uni = properties.getUni(), 
+                        nick =properties.getNick(),
+                        pass=properties.getPass();
+        try {
+            guard.login(uni, nick, pass);
+        } catch (OgameException ex) {
+            guard.o.close();
+            return;
+        }
+            while(true){
+                try {
+                    guard.check();
+                } catch(OgameException ex){
+                    try {
+                        guard.o.logout();
+                        guard.o.login(uni, nick, pass);
+                    } catch (OgameException ex1) {
+                        Logger.getLogger(OgameGuard.class.getName()).log(Level.SEVERE, null, ex1);
+                    }
+                }
+                guard.o.wait(hour, minute, seconds);
+            }
+        
     }
     
     
@@ -68,18 +102,6 @@ public class OgameGuard {
      * @param args the command line arguments are ignored
      */
     public static void main(String[] args) {
-        String uni = "116";
-        String lang= "pl";
-        int hour=0,minute=2,seconds=0;
-        OgameGuard guard = new OgameGuard(Ogame.ogameFabric("uni", "pl"));
-        while(true){
-            try {
-                guard.check();
-            } catch(OgameException ex){
-                
-            }
-            guard.o.wait(hour, minute, seconds);
-        }
-        
+       main1();  
     }
 }
