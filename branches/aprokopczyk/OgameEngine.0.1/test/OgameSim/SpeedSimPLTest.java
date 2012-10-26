30f078f2511d39c6d1a368d4b2018cb95df8cf58
andrzejprokopczyk@gmail.com <andrzejprokopczyk@gmail.com@ac2fc3db-d806-6336-2a2a-3cf1882426c2>
2012-10-26 21:12:02 +0000

    Symulator - wczytanie danych floty, parsowanie wyników w trakcie realizacji.

--- branches/aprokopczyk/OgameEngine.0.1/test/OgameSim/SpeedSimPLTest.java
+++ branches/aprokopczyk/OgameEngine.0.1/test/OgameSim/SpeedSimPLTest.java
@@ -8,6 +8,7 @@ import OgameElements.BattleResult;
 import OgameElements.Fleet;
 import OgameElements.FleetAndDefence;
 import OgameElements.Ships;
+import OgameElements.Defence;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import org.junit.After;
@@ -23,22 +24,22 @@ import org.junit.Ignore;
  * @author andrzej
  */
 public class SpeedSimPLTest {
-    
+
     public SpeedSimPLTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -77,7 +78,45 @@ public class SpeedSimPLTest {
         System.out.println("sim");
         Fleet f1 = new Fleet();
         f1.add(Ships.MT, 3);
+        f1.add(Ships.DT, 4);
+        f1.add(Ships.LM, 5);
+        f1.add(Ships.CM, 3);
+        f1.add(Ships.KR, 4);
+        f1.add(Ships.OW, 5);
+        f1.add(Ships.KOL, 3);
+        f1.add(Ships.REC, 4);
+        f1.add(Ships.SOND, 5);
+        f1.add(Ships.BOMB, 3);
+        f1.add(Ships.NISZ, 4);
+        f1.add(Ships.GS, 5);
+        f1.add(Ships.PAN, 3);
+        
         FleetAndDefence f2 = new FleetAndDefence();
+        f2.add(Ships.MT, 1);
+        f2.add(Ships.DT, 2);
+        f2.add(Ships.LM, 3);
+        f2.add(Ships.CM, 4);
+        f2.add(Ships.KR, 1);
+        f2.add(Ships.OW, 2);
+        f2.add(Ships.KOL, 3);
+        f2.add(Ships.REC, 4);
+        f2.add(Ships.SOND, 1);
+        f2.add(Ships.BOMB, 2);
+        f2.add(Ships.NISZ, 3);
+        f2.add(Ships.GS, 4);
+        f2.add(Ships.PAN, 1);
+        f2.add(Ships.SAT,4);
+        
+        
+        f2.add(Defence.DUZA_POWLOKA, 1);
+        f2.add(Defence.DUZY_LASER, 2);
+        f2.add(Defence.DZIALO_GAUSSA, 3);
+        f2.add(Defence.DZIALO_JONOWE, 4);
+        f2.add(Defence.MALA_POWLOKA, 1);
+        f2.add(Defence.MALY_LASER, 2);
+        f2.add(Defence.PRZECIWRAKIETA, 3);
+        f2.add(Defence.WYRZUTNIA_PLAZMY, 4);
+        f2.add(Defence.WYRZUTNIA_RAKIET,5);
         Sim instance = new SpeedSimPL();
         BattleResult expResult = null;
         instance.start();
