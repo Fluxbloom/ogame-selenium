8baf4d3e4eafa19f2ac342a51570a224181d6fd2
dyschemist <dyschemist@ac2fc3db-d806-6336-2a2a-3cf1882426c2>
2012-10-31 07:26:50 +0000

    mały kommit

--- tags/OgameEngine.0.1/test/NewFarming.java
+++ tags/OgameEngine.0.1/test/NewFarming.java
@@ -41,55 +41,30 @@ public class NewFarming extends OgameTest {
     public void farm() throws OgameException {
 
         Coords[] cNami = Coords.parseArray(new String[]{
-            "2:42:5","2:43:4","2:43:6","2:45:9","2:46:9","2:46:10",
-            "2:47:4","2:48:8",
-            "2:50:6","2:50:7","2:50:10",
-            "2:52:4","2:54:6","2:54:7","2:55:10","2:56:11","2:56:12",
-            "2:58:12","2:59:11","2:61:10","2:62:6",
-            "2:64:10","2:64:11","2:65:12",
-            "2:67:4","2:67:7","2:67:9","2:69:7",
-            "2:74:10","2:74:11"
+            "2:62:5","2:59:11","2:57:4","2:56:12","2:56:11","2:54:8","2:54:6","2:52:6","2:50:12","2:50:11","2:50:8","2:49:7","2:48:8","2:47:4",
+            "2:46:5","2:45:6"
                 });
         Coords[] cAisha = Coords.parseArray(new String[]{
-                    "2:91:6","2:97:4", "2:97:10","2:103:12", "2:105:7","2:105:12",
-                    "2:107:9", "2:107:10", "2:107:12","2:108:7","2:112:6",
-                    "2:114:6", "2:114:11", "2:114:12", "2:115:6", "2:115:7",
-                    "2:118:11", "2:119:12", "2:122:10"
-                //,"2:116:4","2:97:6", "2:97:11","2:103:8"
+                    "2:119:4","2:118:12","2:118:5","2:116:12","2:116:4","2:115:7","2:115:6","2:113:6","2:112:6","2:112:4","2:109:11","2:107:12",
+                    "2:105:7","2:97:4","2:97:6","2:97:11","2:103:8"
                 });
         Coords[] cVidel = Coords.parseArray(new String[]{
-                    "2:155:4","2:156:5", 
-                    "2:157:7", "2:157:10",
-                    "2:159:4","2:159:5", "2:159:11",
-                   "2:161:12", "2:163:6","2:169:9", "2:169:11", "2:169:12",
-                    "2:170:4","2:170:6","2:170:11", "2:171:6","2:174:4"
+                    "2:171:4","2:170:6","2:170:4","2:169:12","2:169:11","2:169:9","2:168:10","2:163:6","2:168:5","2:161:12","2:157:7",
+                    "2:151:4","2:146:10"
                 });
         Coords[] cHinata = Coords.parseArray(new String[]{
-           "2:188:10",
-            "2:193:8","2:197:4","2:205:12","2:207:12", "2:208:7",
-            "2:211:4","2:213:12","2:214:5"
+           "2:212:12","2:211:6","2:205:12","2:196:12","2:193:6"
                 });
         Coords[] cTatsuki = Coords.parseArray(new String[]{
-         "2:224:6","2:224:10","2:228:6","2:228:7","2:229:10"   ,
-         "2:230:6","2:234:5","2:235:4","2:235:5",
-         "2:241:6","2:241:11",
-        "2:247:4","2:258:4","2:250:3","2:250:11","2:251:11"//,"2:224:5","2:250:12" //obrona
+         "2:257:10","2:254:10","2:251:12","2:251:11","2:251:6","2:250:12","2:250:11","2:250:3","2:249:12","2:247:4","2:241:6","2:235:9",
+         "2:234:5"
                 });
         Coords[] cHotaru = Coords.parseArray( new String[]{
-                "3:18:10","3:18:11","3:19:7","3:20:12",
-                "3:21:8","3:21:11","3:21:12",
-                "3:25:7",
-                "3:26:6",
-                "3:28:8",
-                "3:29:9",
-                "3:29:10","3:30:4", "3:30:9",
-                "3:34:6","3:34:8",//"3:34:10",
-                "3:35:5","3:35:8","3:35:12",//"3:37:6",
-                "3:39:6","3:39:7","3:40:6","3:40:8","3:41:10",
-               "3:43:6","3:43:8","3:44:9"});
+                "3:44:9","3:38:12","3:38:11","3:38:9","3:38:7","3:32:10","3:32:5","3:31:13","3:30:11","3:29:9","3:28:8","3:26:6","3:25:7",
+        "3:20:12"});
         
         Coords[] cErza = Coords.parseArray(new String[]{
-           "4:337:6","4:339:12","4:340:12","4:340:8",
+             "4:347:4","4:346:10","4:340:12","4:340:8"
         });
         Fleet fs = new Fleet(), farming = new Fleet(),exp = new Fleet();
         fs.add(Ships.LM, Ships.ALL);
@@ -99,14 +74,14 @@ public class NewFarming extends OgameTest {
         fs.add(Ships.PAN, Ships.ALL);
         farming.add(Ships.MT, 5);
         exp.add(Ships.DT, 100);
-        PlanetElement aisha = new PlanetElement(new Planet(new Coords(2, 106, 6), "Aisha"), fs, 1,false);
-        PlanetElement videl = new PlanetElement(new Planet(new Coords(2, 158, 7), "Videl"), fs, 1,false);
-        PlanetElement hinata = new PlanetElement(new Planet(new Coords(2, 199, 7), "Hinata"), fs, 1,false);
-        PlanetElement tatsuki = new PlanetElement(new Planet(new Coords(2, 244, 7), "Tatsuki"), fs, 1,false);
-        PlanetElement nami = new PlanetElement(new Planet(new Coords(2, 57, 11), "Nami"), fs, 1,false);
-        PlanetElement hotaru = new PlanetElement(new Planet(new Coords(3,33,5),"Hotaru"),fs,1,false);
-        PlanetElement erza = new PlanetElement(new Planet(new Coords(4,344,8),"Erza"),fs,1,false);
-        Farmer f = new Farmer(aisha,1); // ustawiamy na planete z sondami + 1 wolny slot zostawiamy
+        PlanetElement aisha = new PlanetElement(new Planet(new Coords(2, 106, 6), "Aisha"), fs, 1);
+        PlanetElement videl = new PlanetElement(new Planet(new Coords(2, 158, 7), "Videl"), fs, 1);
+        PlanetElement hinata = new PlanetElement(new Planet(new Coords(2, 199, 7), "Hinata"), fs, 1);
+        PlanetElement tatsuki = new PlanetElement(new Planet(new Coords(2, 244, 7), "Tatsuki"), fs, 1);
+        PlanetElement nami = new PlanetElement(new Planet(new Coords(2, 57, 11), "Nami"), fs, 1);
+        PlanetElement hotaru = new PlanetElement(new Planet(new Coords(3,33,5),"Hotaru"),fs,1);
+        PlanetElement erza = new PlanetElement(new Planet(new Coords(4,344,8),"Erza"),fs,1);
+        Farmer f = new Farmer(aisha); // ustawiamy na planete z sondami + 1 wolny slot zostawiamy
         f.setStationarySonds(true);
         f.add(aisha, cAisha, farming);
         f.add(videl, cVidel, farming);
